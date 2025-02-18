ad2e494e18c6fff1c81c64426f4be04740afed4e
mdanyllo <markusdanyllo07@gmail.com>
2025-02-17 23:37:40 -0300

    official commit 0

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
-function Mensagem() {
-     alert('Sua mensagem foi enviada!');
-}
+document.getElementById("meuFormulario").addEventListener("submit", async function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch('https://apidp-production.up.railway.app/sendEmail', { 
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+
+    const result = await response.json();
+    alert(result.success || result.error);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll(".a_nav_bar").forEach(anchor => {
+        anchor.addEventListener("click", function (event) {
+            event.preventDefault();
+            
+            const targetId = this.getAttribute("href").substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
+});
+
+        const slides = document.querySelectorAll(".carousel img");
+        const totalSlides = slides.length;
+        let index = Math.floor(totalSlides / 2);
+
+        function updateCarousel() {
+            slides.forEach((slide, i) => {
+                if (i === index) {
+                    slide.classList.add("active");
+                } else {
+                    slide.classList.remove("active");
+                }
+            });
+        }
+
+        function nextSlide() {
+            index = (index + 1) % totalSlides;
+            updateCarousel();
+        }
+
+        function prevSlide() {
+            index = (index - 1 + totalSlides) % totalSlides;
+            updateCarousel();
+        };
+
+        updateCarousel();8
